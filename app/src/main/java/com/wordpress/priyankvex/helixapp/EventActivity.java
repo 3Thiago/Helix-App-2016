efffb534d738150582cc3b793571bff0c0d5e4bb
Priyank Verma <priyankvex95@gmail.com>
2016-02-17 17:40:19 +0530

    Add event activity layout.

---
+++ app/src/main/java/com/wordpress/priyankvex/helixapp/EventActivity.java
@@ -0,0 +1,39 @@
+package com.wordpress.priyankvex.helixapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by @priyankvex on 17/2/16.
+ * Activity to display the event info.
+ */
+public class EventActivity extends AppCompatActivity{
+
+    private FloatingActionButton buttonInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        buttonInfo = (FloatingActionButton) findViewById(R.id.fab);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_event, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_register){
+            // start register activity
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- app/src/main/java/com/wordpress/priyankvex/helixapp/EventsFragment.java
+++ app/src/main/java/com/wordpress/priyankvex/helixapp/EventsFragment.java
@@ -1,14 +1,19 @@
 package com.wordpress.priyankvex.helixapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.wordpress.priyankvex.helixapp.DataHelpers.EventsSeed;
+import com.wordpress.priyankvex.helixapp.Model.Event;
+
+import java.util.ArrayList;
 
 /**
  * Created by priyank on 6/2/16.
@@ -16,12 +21,24 @@ import com.wordpress.priyankvex.helixapp.DataHelpers.EventsSeed;
  */
 public class EventsFragment extends Fragment{
 
+    private GridView gridViewEvents;
+    private ArrayList<Event> mEvents;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView =  inflater.inflate(R.layout.fragment_events, container, false);
-        GridView gridView = (GridView) rootView.findViewById(R.id.gridView);
-        EventsGridAdapter adapter = new EventsGridAdapter(getActivity(), EventsSeed.getEvents());
-        gridView.setAdapter(adapter);
+        gridViewEvents = (GridView) rootView.findViewById(R.id.gridView);
+        mEvents = EventsSeed.getEvents();
+        EventsGridAdapter adapter = new EventsGridAdapter(getActivity(), mEvents);
+        gridViewEvents.setAdapter(adapter);
+        gridViewEvents.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getActivity(), EventActivity.class);
+                i.putExtra("eventCode", position);
+                startActivity(i);
+            }
+        });
         return rootView;
     }
 }
