7fb1b54c78dede5952c4b8470aed14bc89f89ae0
Priyank Verma <priyankvex95@gmail.com>
2016-02-17 18:19:21 +0530

    Update events data seed.

--- app/src/main/java/com/wordpress/priyankvex/helixapp/DataHelpers/EventsSeed.java
+++ app/src/main/java/com/wordpress/priyankvex/helixapp/DataHelpers/EventsSeed.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
  */
 public class EventsSeed {
 
+
+    public static String registerationLink = "https://docs.google.com/forms/d/16oJHtBey5Jnu6C0_06rayXXNl0dY-YshMC0CnIgMncM/formResponse";
+
     /**
      * Returns list of events
      * @return array list of events
@@ -23,41 +26,107 @@ public class EventsSeed {
         event1.setEventId(1);
         event1.setEventName("App Development");
         event1.setEventImage(R.drawable.mobileapp);
+        event1.setEventSubTitle("Bring out the creator in you");
+        event1.setEventDetails("This is an application development competition. Participants can have individual participation or group participation. The winner will be decided by beta testing of the app, i.e., the participant(s) will have to promote his/her/their app so that people install it on their devices and give their reviews or rating.");
+        event1.setLevelNote("Level Registrations will close on the day before the starting of level 1");
+        event1.setMoreInfoLink("https://drive.google.com/file/d/0B5iiTEqAQuR5Z1IwTUotQmZmODA/view");
+        event1.setLevelOne("Submission of SDLC and judgment- 22th feb.");
+        event1.setLevelTwo("Live demo of working of the App(terminal run) - 1st March.");
+        event1.setLevelThree("Live demo of working of the App(terminal run) - 1st March.");
+        event1.setLevelFour("");
 
         Event event2 = new Event();
         event2.setEventId(2);
         event2.setEventName("My Smart City Jabalpur");
         event2.setEventImage(R.drawable.smart_city);
+        event2.setEventSubTitle("Your ideas. Your smart city");
+        event2.setEventDetails("A platform for the aspiring engineers to paint the canvas of their city with their innovative ideas. Ideas, which can promote economic opportunities, better governance and provide physical, social & economical infrastructure in Jabalpur");
+        event2.setLevelNote("Level Registrations will close on the day before the starting of level 1");
+        event2.setMoreInfoLink("https://drive.google.com/file/d/0B5iiTEqAQuR5NFBDbm13aDNjc3M/view");
+        event2.setLevelOne("Submission of idea (ppt) and judgement - 24th February");
+        event2.setLevelTwo("Iteration and enhancements submission and judgement- 3rd March");
+        event2.setLevelThree("12th/13th March (during the event at JEC)");
+        event2.setLevelFour("");
 
         Event event3 = new Event();
         event3.setEventId(3);
         event3.setEventName("Build A Business");
         event3.setEventImage(R.drawable.startup);
+        event3.setEventSubTitle("Ready. Get set. \"Start Up.\"");
+        event3.setEventDetails("Time to portray your ideas to the world. In the spirit of promoting entrepreneurship in our society, we aim at giving young entrepreneurs a platform to showcase their business ideas that generate a social impact with financial sustainability. Inspired by the recent Startup India mission of our Prime Minister, we put young minds to a scaffold through this competition by letting them explore their local/global market and grab the opportunities in our country which currently has the third largest number of startups in the world and an immensely fast growing economy.");
+        event3.setLevelNote("Level Registrations will close on the day before the starting of level 1");
+        event3.setMoreInfoLink("https://drive.google.com/file/d/0B5iiTEqAQuR5NXh6b3hxM3NTTFU/view");
+        event3.setLevelOne("Submission of idea (ppt) and judgement - 23rd February.");
+        event3.setLevelTwo("Iteration and enhancements submission and judgement- 2nd March");
+        event3.setLevelThree("Teams are required to complete a task given in Level 2. – 12th/13th March (during the event at JEC)");
+        event3.setLevelFour("");
 
         Event event4 = new Event();
         event4.setEventId(4);
         event4.setEventName("Round Table Conclave");
         event4.setEventImage(R.drawable.panel);
+        event4.setEventSubTitle("Your opinion MATTERS!");
+        event4.setEventDetails("This event will host a number of renowned personalities as our panelists. They will exchange their ideas and discussvarious technological aspects, by asking questions or reacting to the opinions of other panel members. This will be followed by a question and answer session by the students. This event is directed towards providing a useful way to trigger an exchange of viewpoints among experts for the awareness and upliftment of our society.");
+        event4.setLevelNote("");
+        event4.setMoreInfoLink("");
+        event4.setLevelOne("");
+        event4.setLevelTwo("");
+        event4.setLevelThree("");
+        event4.setLevelFour("");
+
 
         Event event5 = new Event();
         event5.setEventId(5);
-        event5.setEventName("Photography");
+        event5.setEventName("Humans of JEC");
         event5.setEventImage(R.drawable.photography);
+        event5.setEventSubTitle("A pictorial story of life at JEC.");
+        event5.setEventDetails("The quintessence of a scenario lies in the eyes of the one who pictures it from one's own imagination. And there is no better way of demonstrating that beauty than capturing it through the eyes of your camera lens.\n" +
+                "Helix gives you a chance to bring out your unique visions to the world through the art of photography. Let the silence speak, let the stills move into life.");
+        event5.setLevelNote("The Photo Contest begins at 12:00am Feb 11, 2016 and ends at 12:00am on March 11, 2016. Entries submitted before or after the Entry Period will not be eligible. All images must be submitted though Email: helix@jecjabalpur.ac.in or through our Facebook Page www.facebook.com/helixjec, including all required fields.");
+        event5.setMoreInfoLink("https://drive.google.com/file/d/0B5iiTEqAQuR5TFpEcTVCdGUtcE0/view");
+        event5.setLevelOne("");
+        event5.setLevelTwo("");
+        event5.setLevelThree("");
+        event5.setLevelFour("");
 
         Event event6 = new Event();
         event6.setEventId(6);
         event6.setEventName("Code Conclave");
         event6.setEventImage(R.drawable.code_event);
+        event6.setEventSubTitle("Be Greedy ! Be Brutal !");
+        event6.setEventDetails("Code Conclave embraces the real scrutiny of your programming prowess. Fortify yourself to race against time. You will be tested for your competence in your programming, coordination and ability to visualise in the peak of time.");
+        event6.setLevelNote("");
+        event6.setMoreInfoLink("https://drive.google.com/file/d/0B5iiTEqAQuR5QlFrSDBWLUd4eHM/view");
+        event6.setLevelOne("Eliminator - 25th February");
+        event6.setLevelTwo("Knock Out – 29th Febuary");
+        event6.setLevelThree("Coding Finale - 12th/13th March (during the event at JEC)");
+        event6.setLevelFour("");
 
         Event event7 = new Event();
         event7.setEventId(7);
         event7.setEventName("Mock Campus");
         event7.setEventImage(R.drawable.job);
+        event7.setEventSubTitle("You are HIRED!");
+        event7.setEventDetails("Mock Campus will provide a platform to the students for their SWOT analysis in terms of their preparation for the upcoming campus drives. The mock campus simulates exactly the company recruitment process with the same vigor and rigour. This mock will have a written test, interview and group discussion.");
+        event7.setLevelNote("");
+        event7.setMoreInfoLink("https://drive.google.com/file/d/0B5iiTEqAQuR5WWJFaHE3VjVzdDA/view");
+        event7.setLevelOne("Written Test- 4th March 2016");
+        event7.setLevelTwo("Group Discussion - 7th March 2016");
+        event7.setLevelThree("Case Study/ Picture Perception/ Pseudo Codes Decoding / Video Synthesis/ - 9th March 2016");
+        event7.setLevelFour("Technical/ Management/ HR interviews - 11th /12th /13th March");
 
         Event event8 = new Event();
         event8.setEventId(8);
         event8.setEventName("Mock UPSC");
         event8.setEventImage(R.drawable.upsc);
+        event8.setEventSubTitle("The Administrator!");
+        event8.setEventDetails("Mock UPSC will test the aspirants preparing for civil services and avail them with an opportunity to judge their preparations at all three stages. This event will comprise of the preliminary test, mains and personal interview (by a panel of senior IAS/IPS).");
+        event8.setLevelNote("");
+        event8.setMoreInfoLink("https://drive.google.com/file/d/0B5iiTEqAQuR5VGlLRXdBeWZXOFE/view");
+        event8.setLevelOne("Prelims - 6th March");
+        event8.setLevelTwo("Mains - 10th March");
+        event8.setLevelThree("Personal Interview 12th/13th March(during the event at JEC)");
+        event8.setLevelFour("");
 
         events.add(event1);
         events.add(event2);
--- app/src/main/java/com/wordpress/priyankvex/helixapp/Model/Event.java
+++ app/src/main/java/com/wordpress/priyankvex/helixapp/Model/Event.java
@@ -9,6 +9,15 @@ public class Event {
     private String eventName;
     private int eventImage;
     private int eventId;
+    private String eventSubTitle;
+    private String eventDetails;
+    private String levelNote;
+    private String levelOne;
+    private String levelTwo;
+    private String levelThree;
+    private String levelFour;
+    private String registrationLink;
+    private String moreInfoLink;
 
     public String getEventName() {
         return eventName;
@@ -32,5 +41,77 @@ public class Event {
 
     public void setEventId(int eventId) {
         this.eventId = eventId;
+    }
+
+    public String getEventSubTitle() {
+        return eventSubTitle;
+    }
+
+    public void setEventSubTitle(String eventSubTitle) {
+        this.eventSubTitle = eventSubTitle;
+    }
+
+    public String getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(String eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
+    public String getLevelNote() {
+        return levelNote;
+    }
+
+    public void setLevelNote(String levelNote) {
+        this.levelNote = levelNote;
+    }
+
+    public String getLevelOne() {
+        return levelOne;
+    }
+
+    public void setLevelOne(String levelOne) {
+        this.levelOne = levelOne;
+    }
+
+    public String getLevelTwo() {
+        return levelTwo;
+    }
+
+    public void setLevelTwo(String levelTwo) {
+        this.levelTwo = levelTwo;
+    }
+
+    public String getLevelThree() {
+        return levelThree;
+    }
+
+    public void setLevelThree(String levelThree) {
+        this.levelThree = levelThree;
+    }
+
+    public String getRegistrationLink() {
+        return registrationLink;
+    }
+
+    public void setRegistrationLink(String registrationLink) {
+        this.registrationLink = registrationLink;
+    }
+
+    public String getMoreInfoLink() {
+        return moreInfoLink;
+    }
+
+    public void setMoreInfoLink(String moreInfoLink) {
+        this.moreInfoLink = moreInfoLink;
+    }
+
+    public String getLevelFour() {
+        return levelFour;
+    }
+
+    public void setLevelFour(String levelFour) {
+        this.levelFour = levelFour;
     }
 }
